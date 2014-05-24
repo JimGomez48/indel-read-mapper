80077b58942892dcae22703349927c163f3faf36
James Gomez <jim.gomez.48@gmail.com>
2014-05-24 15:45:40 -0700

    WIP

--- read_mapper.py
+++ read_mapper.py
@@ -2,10 +2,19 @@ __author__ = 'james'
 
 
 class ReadMapper:
+    __subseq_map__ = {}
+    __read_map__ = {}
+
     def __init__(self, ref):
-        self.reference = ref
-        self. read_map = {}
+        self.__build_subsequence_map__(ref)
 
     def map_reads(self, reads):
-        # TODO map reads
+        # TODO map reads using subsequence map
+        return self.__read_map__
+
+    def __get_best_match__(self, ):
+        pass
+
+    def __build_subsequence_map__(self, ref):
+        # TODO build the subsequence map for fast read mapping
         pass
